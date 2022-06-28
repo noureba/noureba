c2e68da94109238f8e7be7da70d4562d6a3540c8
NoureDDine Bachikh <94680573+noureba@users.noreply.github.com>
2022-06-28 10:09:18 +0100

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -8,7 +8,7 @@ function Home() {
   });
   return (
     <div className="home" id="home">
-      <h1 data-aos="fade-right">Junior front end developer</h1>
+      <h1 data-aos="fade-right">front end developer</h1>
       <p data-aos="fade-left">
       My name is Noureddine Bachikh, I am front end developer with experience in back end. Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion of mine and I am confident I would be an excellent addition to your organization. In addition to my knowledge base, I actively seek out new technologies and stay up-to-date on industry trends and advancements.</p>
       <p data-aos="fade-right">
